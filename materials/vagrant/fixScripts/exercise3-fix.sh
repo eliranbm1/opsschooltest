2e5b6adbea8fa4ef4458b46303d8c0b4f3b853dc
eliranbm1 <eliranbenmaor1@gmail.com>
2022-01-06 17:27:49 +0200

    my solution

--- materials/vagrant/fixScripts/exercise3-fix.sh
+++ materials/vagrant/fixScripts/exercise3-fix.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
 #add fix to exercise3 here
+rm /etc/apache2/sites-available/000-default.conf
+
+sudo systemctl restart apache2
--- materials/vagrant/fixScripts/exercise5-fix_server1.sh
+++ materials/vagrant/fixScripts/exercise5-fix_server1.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
 #add fix to exercise5-server1 here
+sudo -u vagrant ssh-keygen -t rsa -N "" -f /home/vagrant/.ssh/id_rsa.pub
+
+cp /home/vagrant/.ssh/id_rsa.pub /home/vagrant/.ssh/id_rsa
+
+sudo chown vagrant:vagrant /home/vagrant/.ssh/id_rsa
+
+echo "StrictHostKeyChecking no" >> /home/vagrant/.ssh/config
